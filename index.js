744051dc72ea2845b1ad4b2a3e2ae6fe35d04f09
Pierre Ernst <pierre.ernst@elastic.co>
2020-10-29 17:16:14 -0400

    Implement REST call

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const axios = require('axios');
 
 try {
   const snykOrgId = core.getInput('snyk-org-id');
   const regexTitle = core.getInput('regex-title');
   const regexEmail = core.getInput('regex-email');
   const regexCheckbox = core.getInput('regex-checkbox');
-  console.log(`snykOrgId ${snykOrgId}!`);
-  console.log(`regexTitle ${regexTitle}!`);
-  console.log(`regexEmail ${regexEmail}!`);
-  console.log(`regexCheckbox ${regexCheckbox}!`);
-  core.setOutput("http-response-code", 200);
+
+  axios.defaults.baseURL= 'https://snyk.io/api/v1'; 
+  axios.defaults.headers.common['Authorization']= 'token ${{secret.SNYK_TOKEN}}'; 
+  axios.defaults.headers.post['Content-Type']= 'application/json'; 
+  axios.post('/org/${snykOrgId}/invite', {
+    email: 'pierre@evil.org'
+  })
+  .then(function (response) {
+    core.setOutput("http-response-code", response.status);
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+    core.setOutput("http-response-code", error.status);
+  });
+ 
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
