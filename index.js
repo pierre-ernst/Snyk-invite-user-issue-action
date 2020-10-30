a25d9555b16b703c29d83761d7be58841eaef2d2
Pierre Ernst <pierre-ernst@users.noreply.github.com>
2020-10-30 16:03:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,36 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-try {
-  const snykOrgId = core.getInput('snyk-org-id');
-  const regexTitle = new RegExp(core.getInput('regex-title'));
-  const regexEmail = new RegExp(core.getInput('regex-email'));
-  const checkboxPattern = core.getInput('regex-checkbox');
-  
-  if ( regexTitle.test(github.context.payload.issue.title) ) {
-    const emailMatch = regexEmail.exec(github.context.payload.issue.body);
-    if ( emailMatch != null ) {
-      console.log(`email: ${emailMatch[1]}`);
-      if ( checkboxPattern != null && checkboxPattern != '' ) {
-        const matchedCheckbox = github.context.payload.issue.body.match ( new RegExp(checkboxPattern) );
-        if (matchedCheckbox) {
-          console.log(`matched: ${matchedCheckbox[0]}`)
-          const updatedBody = github.context.payload.issue.body.replace( 
-            matchedCheckbox[0], 
-            matchedCheckbox[0].replace('[ ]', '[x]')
-          );
-          console.log(`body: ${updatedBody}`)
-        }
-      }
-    }
-  }
-  
-/*
+function sendInvite(snykOrgId, email) {
   axios.defaults.baseURL= 'https://snyk.io/api/v1'; 
   axios.defaults.headers.common['Authorization']= `token ${process.env.SNYK_TOKEN}`; 
   axios.defaults.headers.post['Content-Type']= 'application/json'; 
   axios.post(`/org/${snykOrgId}/invite`, {
-    email: 'pierre@evil.org'
+    email: `${email}`
   })
   .then(function (response) {
     core.setOutput("http-response-code", response.status);
@@ -42,10 +18,44 @@ try {
     core.setOutput("http-response-code", error.response.status);
     core.setFailed(error.response);
   });
-*/ 
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
+}
+
+function updateIssue(number, body) {
+  const repo = process.env.GITHUB_REPOSITORY.split("/");
+  
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+  octokit.issues.update({
+    repo[0],
+    repo[1],
+    number,
+    body
+  });
+}
+
+try {
+  const snykOrgId = core.getInput('snyk-org-id');
+  const regexTitle = new RegExp(core.getInput('regex-title'));
+  const regexEmail = new RegExp(core.getInput('regex-email'));
+  const checkboxPattern = core.getInput('regex-checkbox');
+  
+  if ( regexTitle.test(github.context.payload.issue.title) ) {
+    const emailMatch = regexEmail.exec(github.context.payload.issue.body);
+    if ( emailMatch != null ) {
+      sendInvite(snykOrgId, emailMatch[1]);
+      
+      if ( checkboxPattern != null && checkboxPattern != '' ) {
+        const matchedCheckbox = github.context.payload.issue.body.match ( new RegExp(checkboxPattern) );
+        if (matchedCheckbox) {
+          const updatedBody = github.context.payload.issue.body.replace( 
+            matchedCheckbox[0], 
+            matchedCheckbox[0].replace('[ ]', '[x]')
+          );
+          
+          updateIssue(github.context.payload.issue.number, updatedBody);
+        }
+      }
+    }
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
-
